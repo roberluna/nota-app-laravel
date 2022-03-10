bf650f8c6dc0527e5cf750f34d242ba413720ce6
= <rcluna@uach.mx>
2022-03-09 19:28:47 -0700

    primera version incluy el crud de notas, tambien aatuenitcacion

---
+++ app/Http/Controllers/NotaController.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Nota;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Auth;
+
+class NotaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+       $notas = Nota::where('users_id',Auth::id())->get();
+
+       return Inertia::render('Notas/Index',[
+           'notas' => $notas
+       ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return Inertia::render('Notas/Create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'titulo' => 'required',
+            'contenido' => 'required',
+        ]);
+
+        $nota = New Nota;
+        $nota->titulo = $request->titulo;
+        $nota->contenido = $request->contenido;
+        $nota->users_id = Auth::id();
+        $nota->save();
+
+        return redirect()->route('nota.index')->with('status','Se creó una nota');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+        return Inertia::render('Notas/Show',[
+            'nota' => Nota::where('id',$id)
+                ->where('users_id',Auth::id())
+                ->first()
+        ]);
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return Inertia::render('Notas/Edit',[
+            'nota' => Nota::where('id',$id)
+                ->where('users_id',Auth::id())
+                ->first()
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+
+         $request->validate([
+            'titulo' => 'required',
+            'contenido' => 'required',
+        ]);
+
+        //select * from notas where id = $id and users_id = $Auth::id()
+
+        $nota = Nota::where('id',$id)
+                ->where('users_id',Auth::id())
+                ->first();
+
+        $nota->update($request->all());
+
+        return redirect()->route('nota.index')->with('status','La nota se actualizó correctamente');
+
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Nota  $nota
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $nota =  Nota::where('id',$id)
+                ->where('users_id',Auth::id())
+                ->first();
+
+        $nota->delete();
+        return redirect()->route('nota.index')->with('status','La nota se eliminó correctamente');
+    }
+}
---
+++ resources/js/Pages/Notas/Create.vue
@@ -0,0 +1,67 @@
+<template>
+    <app-layout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Crear noiticas
+            </h2>
+        </template>
+
+        <div>
+        <div class="max-w-4xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div class="mt-5 md:mt-0 md:col-span-2">
+                <form @submit.prevent="enviar">
+
+                        <label class="block font-medium text-sm text-gray-700">
+	                        Título
+                        </label>
+
+                        <textarea 
+                            class="form-input w-full rounded-md shadow-sm"
+                            v-model="form.titulo"
+                        ></textarea>
+
+                        <label class="block font-medium text-sm text-gray-700">
+	                        Contenido
+                        </label>
+
+                        <textarea 
+                            class="form-input w-full rounded-md shadow-sm"
+                            v-model="form.contenido"
+                            rows="8"
+                        ></textarea>
+
+                        <button 
+                            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md"
+                        >Crear</button>
+
+                    </form>
+            </div>
+        </div>
+    </div>
+    </app-layout>
+</template>
+
+<script>
+    import { defineComponent } from 'vue'
+    import AppLayout from '@/Layouts/AppLayout.vue'
+
+    export default defineComponent({
+        components: {
+            AppLayout,
+        },
+        data () {
+            return {
+                form: {
+                    titulo: '',
+                    contenido: ''
+                }
+            }
+        },
+        methods: {
+            enviar() {
+                this.$inertia.post(this.route('nota.store'), this.form)
+            }
+        }
+
+    })
+</script>
